8161ad1206e5c1784c8c53b4ad1578a2ea30fce3
Bjorn-Olav Strand <bolav@ikke.no>
2016-10-27 22:26:05 +0200

    wrapper for web

--- fuse-firebase.js
+++ fuse-firebase.js
@@ -35,6 +35,7 @@ function Element(args) {
 	this._name = 'Element';
 	this._tag = '_unk';
 	this.nodeType = 1;
+	this.style = {};
 	this.attributes = {};
 
 	var keys = Object.keys(args);
@@ -57,29 +58,47 @@ function Element(args) {
 }
 
 Element.prototype.appendChild = function appendChild( elem ) {
-	console.log("appendChild to " + this._tag + "(" + elem._tag + ")" );
+	// console.log("appendChild to " + this._tag + "(" + elem._tag + ")" );
 	this.childNodes.push(elem);
-	this._setChildren();
+
+	if (elem._tag.toLowerCase() === "iframe") {
+		elem.document = new Element({_name: "IFrameDocument", _tag:"_iframe_document"});
+		elem.document.body = new Element({_name: "IFrameDocumentBody", _tag:"_iframe_document_body"});
+		elem.document.open = function () {
+		};
+		elem.document.write = function () {
+		};
+		elem.document.close = function () {
+		};
+		elem.readyState = "complete";
+	}
 
 	if (elem._tag.toLowerCase() === "script") {
 		if (elem.src) {
-			console.log("Loading " + elem.src);
+			// console.log("Loading " + elem.src);
 			fetch(elem.src).then(function (response) {
 				return response.text();
 			}).then(function (resp_text) {
-				console.log("Evalling " + resp_text);
+				var found = resp_text.match(/(function pLPCommand[\S\s]+function pRTLPCB[^\}]+\})/);
+				if (found) {
+					this._eval_header = 'var parent = { window: window }; ' + found[1];
+					resp_text = 'var parent = { window: window }; ' + resp_text;
+				}
+				else if (this._eval_header) {
+					resp_text = this._eval_header + "\n" + resp_text;
+				}
 				eval(resp_text);
+
+				if (typeof elem.onreadystatechange === "function") {
+					// loadScript(elem.src, elem.src);
+					// console.log(elem.onreadystatechange.toString());
+					elem.onreadystatechange();
+				}
 			}).catch(function (err) {
 				debug_log('CATCH: ' + err);
+				console.log(resp_text);
 			});
 		}
-	}
-	if (typeof elem.onreadystatechange === "function") {
-		// loadScript(elem.src, elem.src);
-		console.log("Running onreadystatechange");
-		console.log(JSON.stringify(elem));
-		console.log(elem.onreadystatechange.toString());
-		elem.onreadystatechange();
 	}
 
 	elem.parentNode = this;
@@ -87,7 +106,7 @@ Element.prototype.appendChild = function appendChild( elem ) {
 }
 
 Element.prototype.removeChild = function removeChild( elem ) {
-	console.log("removeChild from " + this._tag + "(" + elem._tag + ")" );
+	// console.log("removeChild from " + this._tag + "(" + elem._tag + ")" );
 	var i = this.childNodes.indexOf(elem);
 	if (i >= 0) {
 		this.childNodes.splice(i, 1);
@@ -95,8 +114,8 @@ Element.prototype.removeChild = function removeChild( elem ) {
 }
 
 Element.prototype.setAttribute = function setAttribute (name,val) {
-	console.log(JSON.stringify(arguments));
-	console.log("Element " + this._tag + " setAttribute " + name + ", " + val);
+	// console.log(JSON.stringify(arguments));
+	// console.log("Element " + this._tag + " setAttribute " + name + ", " + val);
 	this.attributes[name] = val;
 };
 
@@ -118,10 +137,11 @@ var getElementsByTagName = function getElementsByTagName (tag, ret) {
 
 Element.prototype.getElementsByTagName = getElementsByTagName;
 
-document = new Element({_name: "Window", _tag:"Base"});;
+document = new Element({_name: "Window", _tag:"Base"});
+document.body = new Element({_name: "Body", _tag:"body"});
 
 Element.prototype.createElement = function createElement (elem) {
-	console.log("createElement: " + JSON.stringify(arguments));
+	// console.log("createElement: " + JSON.stringify(arguments));
 	return new Element({ _tag: elem });
 }
 
@@ -140,8 +160,11 @@ this.addEventListener = function(type, listener, p) {
 	_events[type].push(listener);
 };
 
+document.readyState = "complete";
+document.addEventListener = this.addEventListener;
+
 firebase = require('/node_modules/firebase/app');
-require('/node_modules/firebase/database-node');
+require('/node_modules/firebase/database');
 require('/node_modules/firebase/auth');
 require('/node_modules/firebase/storage');
 firebase.database.INTERNAL.forceLongPolling();
